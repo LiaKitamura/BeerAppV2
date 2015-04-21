c11aabe982e835716a46064bd0ab9d3979c5da6b
LiaKitamura <kit4mura@gmail.com>
2015-04-20 21:51:40 -0600

    trying to add beer info page

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -3,17 +3,22 @@
   angular.module('IHeartBeer', ['ngRoute', 'ngResource'])
     .config(function($routeProvider){
       $routeProvider
-       
+
         .when('/style/:styleId/:page', {
           templateUrl: '/assets/templates/pages/style.html',
           controller: 'StyleIndexController',
         })
-        
+
         .when('/style/:styleId/', {
           templateUrl: '/assets/templates/pages/style.html',
           controller: 'StyleIndexController',
         })
-      
+
+        .when('/beer/:beerId/', {
+          templateUrl: '/assets/templates/pages/beers/index.html',
+          controller: 'BeerIndexController',
+        })
+
     });
 
 })();
--- app/assets/js/controllers/beersIndexController.js
+++ app/assets/js/controllers/beersIndexController.js
@@ -1,5 +1,5 @@
 angular.module('IHeartBeer').controller('BeersIndexController', ['Beer','$routeParams', '$scope', function(Beer, $routeParams, $scope){
 
-  	$scope.beers = Beer.query({styleId: $routeParams.styleId, page: $routeParams.page });
+  	$scope.beer = Beer.get({beerId: $routeParams.beerId });
 
 }]);
